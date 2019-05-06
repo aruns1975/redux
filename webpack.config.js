a86dce068c278d1937a134b05f561827df44b59d
Arun Subramonia Sarma <aruns1975@mail.com>
2019-05-06 06:33:15 +0530

    redux-saga

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+           loader:'babel-loader',
+           options: { presets: ['es2015'] }
+        },
+        exclude: /node_modules/
+     }
+   ]
   }
 };
